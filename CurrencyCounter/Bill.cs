e57235736cb79b5d8fcfad27b13f6ea8be4f661b
Julia Kropyvna <ykrop@softserveinc.com>
2019-10-17 17:01:18 +0300

    Add smth

---
+++ CurrencyCounter/Bill.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyCounter
+{
+    public class Bill
+    {
+        public int Greenbacks { get; set; }
+
+        public decimal Coins { get; set; }
+
+        public Bill(int cash, decimal coins)
+        {
+            Greenbacks = cash;
+            Coins = coins;
+        }
+    }
+}
