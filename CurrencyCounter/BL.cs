8e7879b3bd67e5b2d107ae022e58ab1098c0896f
Julia Kropyvna <ykrop@softserveinc.com>
2019-10-24 17:42:27 +0300

    Add entity

--- CurrencyCounter/BL.cs
+++ CurrencyCounter/BL.cs
@@ -6,40 +6,35 @@ namespace CurrencyCounter
 {
     public class BL
     {
-        public static Dictionary<double, string> exchange;
-
-        public BL()
+        public static List<Currency> exchange = new List<Currency>
         {
-            exchange = new Dictionary<double, string>
-            {
-                {1, "USD"},
-                {0.90, "EUR"},
-                {63.97, "RUB"},
-                {0.78, "GBP"},
-                {0.99, "CHF"},
-                {3.85, "PLN"},
-                {108.54, "JPY"},
-                {24.75, "UAH"},
-                {1.31, "CAD"},
-                {1.46, "AUD"},
-                {2.97, "GEL"},
-                {17.40, "MDL"},
-                {7.09, "CNY"},
-                {6.72, "DKK"},
-                {9.18, "NOK"},
-                {9.71, "SEK"},
-                {2.04, "BYR"},
-                {23.06, "CZK"},
-                {124.55, "ISK"},
-                {9.67, "SKK"},
-                {3.54, "ILS"},
-                {389.70, "KZT"},
-                {297.83, "HUF"},
-                {0.61, "LVL"},
-                {2.95, "LTL"},
-                {1.37, "SGD"}
-            };
-        }
+            new Currency(1, "USD"),
+            new Currency(0.90, "EUR"),
+            new Currency(63.97, "RUB"),
+            new Currency(0.78, "GBP"),
+            new Currency(0.99, "CHF"),
+            new Currency(3.85, "PLN"),
+            new Currency(108.54, "JPY"),
+            new Currency(24.75, "UAH"),
+            new Currency(1.31, "CAD"),
+            new Currency(1.46, "AUD"),
+            new Currency(2.97, "GEL"),
+            new Currency(17.40, "MDL"),
+            new Currency(7.09, "CNY"),
+            new Currency(6.72, "DKK"),
+            new Currency(9.18, "NOK"),
+            new Currency(9.71, "SEK"),
+            new Currency(2.04, "BYR"),
+            new Currency(23.06, "CZK"),
+            new Currency(124.55, "ISK"),
+            new Currency(9.67, "SKK"),
+            new Currency(3.54, "ILS"),
+            new Currency(389.70, "KZT"),
+            new Currency(297.83, "HUF"),
+            new Currency(0.61, "LVL"),
+            new Currency(2.95, "LTL"),
+            new Currency(1.37, "SGD")
+        };
 
         public Bill ToDollar(Bill bill, string currency)
         {
@@ -49,7 +44,7 @@ namespace CurrencyCounter
             }
 
             var current = (bill.Greenbacks + bill.Coins);
-            var rate = exchange.FirstOrDefault(x => x.Value == currency).Key;
+            var rate = exchange.FirstOrDefault(x => x.Name == currency).Value;
             current = Math.Round((current / rate), 2);
             if (current % 1 == 0)
             {
@@ -70,7 +65,7 @@ namespace CurrencyCounter
                 bill.Coins = 0.0001;
             }
             var current = (bill.Greenbacks + bill.Coins);
-            var rate = exchange.FirstOrDefault(x => x.Value == currency).Key;
+            var rate = exchange.FirstOrDefault(x => x.Name == currency).Value;
             current = Math.Round((current * rate), 2);
             if (current % 1 == 0)
             {
---
+++ CurrencyCounter/Currency.cs
@@ -0,0 +1,14 @@
+﻿namespace CurrencyCounter
+{
+    public class Currency
+    {
+        public Currency(double value, string name)
+        {
+            Value = value;
+            Name = name;
+        }
+        public string Name { get; set; }
+
+        public double Value { get; set; }
+    }
+}
